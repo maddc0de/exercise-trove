16427c5f4247aca2fa6b12c0c5c1fa5b6d3ff170
Madelane <madelanedaz@gmail.com>
2023-06-22 10:54:27 +0100

    added styling attribute to make website responsive on larger devices

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'; //use to route between homepage and exercise-details page
+import { Box } from '@mui/material'; // Box component in Material-UI is a layout and flexible container
+
+import './App.css';
+import Navbar from '../components/navbar/Navbar';
+import Home from '../pages/home/Home';
+import ExerciseDetail from '../pages/exerciseDetail/ExerciseDetail';
+import Footer from '../components/footer/Footer';
 
 const app = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{ width: { xl: '1488px' }}} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 
