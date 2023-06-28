0a559381ba1bb6d044834b24a77a36034ee4c6d1
Madelane <madelanedaz@gmail.com>
2023-06-28 19:00:50 +0100

    move BodyPart component styling to BodyPart.css  and display each body part category from the exerciseDB

--- frontend/src/components/bodyPart/BodyPart.js
+++ frontend/src/components/bodyPart/BodyPart.js
@@ -14,16 +14,22 @@ const BodyPart = ({ item, bodyPart, setBodyPart }) => {
       className="bodyPart-card"
       sx={{
         borderTop: bodyPart === item ? '4px solid #ff2625' : '',
-        backgroundColor: '#fff',
+        background: '#fff',
         borderBottomLeftRadius: '20px',
         width: '270px',
         height: '280px',
         cursor: 'pointer',
         gap: '47px'
       }}
-
+      onClick={() => {
+        setBodyPart(item);
+        
+      }}
     >
-      <img src={Icon} alt="dumbbell" styles={{ width: '40px', height: '40px'}} />
+      <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px'}} />
+      <Typography fontSize={"24px"} fontWeight="bold" color="#3A1212" textTransform="capitalize">
+        {item}
+      </Typography>
     </Stack>
   )
 }
