c2634eca8375fe524714d1d01f236c595c838f15
Madelane <madelanedaz@gmail.com>
2023-07-01 16:48:12 +0100

    fix bug on the data returned by fetch- now returns an array instead of an object

--- frontend/src/components/horizontalScrollBar/HorizontalScrollBar.js
+++ frontend/src/components/horizontalScrollBar/HorizontalScrollBar.js
@@ -2,10 +2,10 @@ import React, { useContext } from 'react';
 import { Box, Typography } from '@mui/material';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
-import './HorizontalScrollBar.css';
 import BodyPart from '../bodyPart/BodyPart';
 import RightArrowIcon from '../../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../../assets/icons/left-arrow.png';
+import './HorizontalScrollBar.css';
 
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
