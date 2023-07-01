1212b6768eda51e200039cf0f252455b711eead2
Madelane <madelanedaz@gmail.com>
2023-07-01 18:17:05 +0100

    call scrollTo method on browser window when a body part component gets clicked

--- frontend/src/components/bodyPart/BodyPart.js
+++ frontend/src/components/bodyPart/BodyPart.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Stack, Typography } from '@mui/material'
-
 import Icon from '../../assets/icons/gym.png'
 import './BodyPart.css';
 
@@ -23,7 +22,7 @@ const BodyPart = ({ item, bodyPart, setBodyPart }) => {
       }}
       onClick={() => {
         setBodyPart(item);
-        
+        window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });  // built in browser API
       }}
     >
       <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px'}} />
