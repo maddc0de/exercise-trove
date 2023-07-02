717d892d3fc5f42f8b4317effcb9606d3a20d750
Madelane <madelanedaz@gmail.com>
2023-07-02 10:23:47 +0100

    add styling for exerciseCard' components' tags and exercises' names

--- frontend/src/components/exerciseCard/ExerciseCard.js
+++ frontend/src/components/exerciseCard/ExerciseCard.js
@@ -2,10 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Stack, Typography } from '@mui/material';
 
+import './ExerciseCard.css';
+
 const ExerciseCard = ({ exercise }) => {
   return (
     <Link className="exercise-card" to={`/exercises/${exercise.id}`}>
       <img src={exercise.gifUrl} alt={exercise.name} loading="lazy"/>
+      <Stack direction="row">
+        <Button className="tag" sx={{ background: '#ffa9a9' }}>
+          {exercise.bodyPart}
+        </Button>
+
+        <Button className="tag" sx={{ background: '#fcc757' }}>
+          {exercise.target}
+        </Button>
+      </Stack>
+      <Typography className="exercise-card-name">
+        {exercise.name}
+      </Typography>
     </Link>
   )
 }
