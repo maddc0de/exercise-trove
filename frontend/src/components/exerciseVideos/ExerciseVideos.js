95e00efeb4fd96e533019ac40946e62cba1bd517
Madelane <madelanedaz@gmail.com>
2023-07-04 13:06:39 +0100

    displays relevant and most rated videos

--- frontend/src/components/exerciseVideos/ExerciseVideos.js
+++ frontend/src/components/exerciseVideos/ExerciseVideos.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
+import './ExerciseVideos.css'
+
 const ExerciseVideos = ({ exerciseVideos, name }) => {
-  console.log(`this is exercise videos: ${exerciseVideos}`)
+  console.log(`this is exercise video 1: ${exerciseVideos[1]}`)
   return (
     <Box sx={{ marginTop: {lg: '200px', xs: '20px'}}}>
-      <Stack justifyContent="flex-start" flexWrap="wrap" alignItems="center"
-        sx={{ flexDirection: { lg: 'row'}, gap: { lg: '110px', xs: '0'} }}
-      >
+      <Typography sx={{ fontSize: { lg: '44px', xs: '25px' } }} fontWeight={700} color="#000" mb="33px">
+        Watch <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>{name}</span> exercise videos
+      </Typography>
+      <Stack justifyContent="flex-start" flexWrap="wrap" alignItems="center" sx={{ flexDirection: { lg: 'row'}, gap: { lg: '100px', xs: '0px'} }}>
         {exerciseVideos?.slice(0,3).map((item, index) => (
         <a
           key={index}
@@ -16,7 +19,15 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
           target="_blank"
           rel="noreferrer"
         >
-
+          <img src={item.video.thumbnails[0].url} alt={item.video.title}/>
+          <Box>
+              <Typography sx={{ fontSize: { lg: '28px', xs: '18px' } }} fontWeight={600} color="#000">
+                {item.video.title}
+              </Typography>
+              <Typography fontSize="14px" color="#000">
+                By: {item.video.channelName}
+              </Typography>
+            </Box>
         </a>
         ))}
       </Stack>
--- frontend/src/pages/exerciseDetail/ExerciseDetail.js
+++ frontend/src/pages/exerciseDetail/ExerciseDetail.js
@@ -15,12 +15,12 @@ const ExerciseDetail = () => {
   useEffect(() => {
     const fetchExercisesData = async () => {
       const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
-      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
 
       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions)
       setExerciseDetail(exerciseDetailData);
   
-      const exerciseVideosData = await fetchData(`https://youtube-search-and-download.p.rapidapi.com/search?query=${exerciseDetailData.name} exercise&hl=en&type=v&sort=r`, youtubeOptions)
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}+exercise&hl=en&type=v&sort=r%2C%20ra`, youtubeOptions)
       setExerciseVideos(exerciseVideosData.contents);
     }
     fetchExercisesData();
